21a0d51880f09c84ca5098da341f603910c85c50
Dinko <dinko.dugec@gmail.com>
2023-02-20 21:31:30 +0100

    adding Admin LTE via composer

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ require __DIR__.'/auth.php';
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', 'AdminsController@index')->name('admin.index');
