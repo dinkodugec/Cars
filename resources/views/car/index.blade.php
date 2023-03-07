f0974075cc4068c959fef510db406def5154b6a6
Dinko <dinko.dugec@gmail.com>
2023-03-07 23:44:02 +0100

    some improvments with eloquent collections and installed debugbar

--- resources/views/car/index.blade.php
+++ resources/views/car/index.blade.php
@@ -9,6 +9,7 @@
                     <figure class="figure">
                         <img src="http://via.placeholder.com/640x360" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
                         <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                        <span class="mx-2">Posted by {{ $car->user->name }} ({{ $car->user->cars->count() }} cars)</span>
                     </figure>
                 <div class="card-body">
                     <h5 class="d-inline"><b>{{ $car->name }}</b> </h5>
@@ -16,10 +17,12 @@
                         <div class="text-muted d-inline">{{ $car->description }}</div>
                     </h5> --}}
                     <p> {{ $car->description }}</p>
+                    <span class="float-right">{{ $car->created_at->diffForHumans() }}</span>
                     <a href="/car/{{ $car->id }}"
                         class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>
             
                 </div>
+                <span class="float-right ml-2">{{ $car->created_at->diffForHumans() }}</span>
            </div>
          <br>
         </div>
